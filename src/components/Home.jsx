311d487a4c0177c954028c0d827111f82aea7106
amerihn <amerihn.art@gmail.com>
2024-08-14 20:08:55 +0700

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import heroimg from "../assets/restauranfood.jpg";
 import imgSalad from "../assets/salad.png";
 import imgBrushetta from "../assets/brucheta.jpg";
 import imgLemon from "../assets/lemon.jpeg";
+import imgReview from "../assets/lemon_mark.png";
 import aboutImg1 from "../assets/about1.png";
 import aboutImg2 from "../assets/about2.png";
 import { Link } from 'react-router-dom';
@@ -74,7 +75,7 @@ export default function Home() {
             {testimonials.map(review => (
             <div className="testi-card" key={review.name}>
               <div className="testi-name">
-                <img src={heroimg} className='testi-img'/>
+                <img src={imgReview} className='testi-img'/>
                 <div>
                   <div className="testi-star">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
